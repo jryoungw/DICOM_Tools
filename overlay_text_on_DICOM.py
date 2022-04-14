8c4640d263fccdecc1f1188c52b9e38256248f63
jryoungw <et2035@naver.com>
2022-04-14 10:48:59 +0900

    Update overlay_text_on_DICOM.py

--- overlay_text_on_DICOM.py
+++ overlay_text_on_DICOM.py
@@ -1,17 +1,17 @@
 from typing import Tuple
 import numpy as np
+import pydicom
 
 def overlay_text(
-                 img:np.ndarray, 
-                 text:str, 
                  org_path:str, 
+                 text:str, 
                  save_path:str, 
                  position_ratio:Tuple[float, float]= (0.1, 0.9),
                  text_thickness:int = 10
                 ) -> None:
-    position = (int(img.shape[1]*position_ratio[0]),int(img.shape[0]*position_ratio[0]))
     org = pydicom.dcmread(org_path)
     npy = org.pixel_array
+    position = (int(npy.shape[1]*position_ratio[0]),int(npy.shape[0]*position_ratio[0]))
     dummy = np.zeros(npy.shape)
     text_img = cv2.putText(dummy, text, position, 0, 3, (255,255,255), text_thickness)
     text_img = ((text_img / 255) * npy.max()).astype(npy.dtype)
